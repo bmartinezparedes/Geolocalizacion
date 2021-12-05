e5a7d0e4490e4e8139cd8ee54af188558f0ba8c4
Braiskiskos <bmartinezparedes@danielcastelao.org>
2021-12-05 15:15:46 +0100

    Consigo mi ubicación

--- app/src/main/java/com/kiskos/geolocalizacion/MapsActivity.kt
+++ app/src/main/java/com/kiskos/geolocalizacion/MapsActivity.kt
@@ -1,7 +1,19 @@
 package com.kiskos.geolocalizacion
 
+import android.Manifest
+import android.annotation.SuppressLint
+import android.app.Activity
+import android.content.pm.PackageManager
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.annotation.RequiresApi
+import androidx.core.app.ActivityCompat
+import androidx.core.app.ActivityCompat.shouldShowRequestPermissionRationale
+import androidx.core.content.ContextCompat
 
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
@@ -13,6 +25,7 @@ import com.kiskos.geolocalizacion.databinding.ActivityMapsBinding
 
 class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
 
+    private val PERMISO_LOCALIZACION: Int=3
     private lateinit var mMap: GoogleMap
     private lateinit var binding: ActivityMapsBinding
 
@@ -37,8 +50,10 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
      * it inside the SupportMapFragment. This method will only be triggered once the user has
      * installed Google Play services and returned to the app.
      */
+    @RequiresApi(Build.VERSION_CODES.N)
     override fun onMapReady(googleMap: GoogleMap) {
         mMap = googleMap
+        comprobarPermisos()
         //Hago visible los botones para apliar y desampliar el mapa
         mMap.uiSettings.isZoomControlsEnabled=true
         //Creo una variable con una latitud y longitud
@@ -48,4 +63,44 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
         //Le digo al mapa que centre la camara en el punto
         mMap.moveCamera(CameraUpdateFactory.newLatLng(centro))
     }
+
+    private fun comprobarPermisos(){
+        when{
+            ContextCompat.checkSelfPermission(this,
+                android.Manifest.permission.ACCESS_FINE_LOCATION)==PackageManager.PERMISSION_GRANTED->{
+                Log.i("Permisos","permiso garantozado")
+                mensajeUsuario("Tienes Permisos")
+                }
+            shouldShowRequestPermissionRationale (Manifest.permission.ACCESS_FINE_LOCATION
+            )->{
+                mensajeUsuario("Da permisos en ajustes")
+            }
+            else->{
+                requestPermissions(arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),PERMISO_LOCALIZACION)
+            }
+        }
+    }
+
+    @SuppressLint("MissingPermission")
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        when(requestCode){
+            PERMISO_LOCALIZACION->{
+                if(grantResults.isNotEmpty() && grantResults[0]==PackageManager.PERMISSION_GRANTED){
+                    mMap.isMyLocationEnabled = true
+                }
+            }
+            else->{
+                super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+            }
+        }
+    }
+
+    fun mensajeUsuario(mensaje:String){
+        Toast.makeText(this,mensaje,Toast.LENGTH_LONG).show()
+    }
+
 }
